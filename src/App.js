5e9fa056263307377782d5876525b07494521b37
Dmitry Shumsky <wumsky@gmail.com>
2018-03-02 02:16:40 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import UISelect from './ui.select.jsx'
+
+import data1 from './data/select1.json'
+import data2 from './data/select1.json'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="test-app">
+          <div className="row">
+            <div className="col col1">
+              <UISelect options={data1}/>
+            </div>
+          </div>
+
+          <div className="row">
+            <UISelect options={data2}/>
+          </div>
+
+          <div className="row">
+            <UISelect options={data1}/>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
